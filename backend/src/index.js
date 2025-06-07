7ba42ee06254699e4b31698553e77c2ec6c07cca
AbdouMdn <madaniabderazak01@gmail.com>
2025-06-07 15:12:33 +0100

    environment setup and folder structure

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+
+app.listen(2025,() => {
+    console.log("server is listening in port 2025")
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='p-8 justify-center items-center mx-auto bg-amber-400'>
+      <p className='text-red-600 font-bold font-sans'>Hello world!</p>
+    </div>
+  )
+}
+
+export default App
